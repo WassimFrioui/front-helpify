c4f43d016d7f85ede10ccd9db4e4da2164cdbadc
Wassim Frioui <wfrioui01@gmail.com>
2025-04-12 13:36:21 +0200

    responsive + delete img + add section

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,74 @@
 import FAQAccordion from "@/components/Faq";
 import Navbar from "@/components/Navbar";
-
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center p-4 md:p-24">
       <Navbar />
 
       <header className="relative flex flex-col items-center justify-center text-center w-full min-h-[70vh] p-4 bg-blue-300 rounded-xl">
-
-        <h1 className="text-6xl text-white font-serif">Trouver les bons <span className="text-blue-950 font-thin">prestataires</span> <br /> pour vos besoins</h1>
-        <div className="flex flex-row items-center justify-center mt-4 w-1/2">
-          <input type="text" placeholder="Que cherchez-vous ? Par exemple : Plombier " className="w-2/3 p-2 rounded-l-sm bg-white" />
-          <button className="bg-blue-950 text-white p-2 rounded-r-sm  transition-colors hover:bg-blue-900">Rechercher</button>
+        <h1 className="text-3xl md:text-6xl text-white font-serif">
+          Trouver les bons <span className="text-blue-950 font-thin">prestataires</span> <br /> pour vos besoins
+        </h1>
+        <div className="flex flex-col md:flex-row items-center justify-center mt-4 w-full md:w-1/2">
+          <input
+            type="text"
+            placeholder="Que cherchez-vous ? Par exemple : Plombier "
+            className="w-full md:w-2/3 p-2 rounded-t-sm md:rounded-l-sm md:rounded-t-none bg-white"
+          />
+          <button className="bg-blue-950 text-white p-2 rounded-b-sm md:rounded-r-sm md:rounded-b-none transition-colors hover:bg-blue-900 w-full md:w-auto">
+            Rechercher
+          </button>
         </div>
       </header>
-      <section className="flex flex-col items-center justify-center w-full h-auto p-4 mt-8">
-        <h1 className="text-4xl font-medium mb-6">Comment ça marche ?</h1>
 
-        <div className="flex flex-row items-center justify-center w-full h-auto text-center">
-          <div className="flex flex-col items-center justify-center w-1/3 p-4">
-            <h2 className="text-2xl font-medium">1. Décrivez votre besoin</h2>
-            <p className="text-gray-700">Décrivez votre besoin en quelques mots pour que nous puissions vous aider au mieux.</p>
+      <section className="flex flex-col items-center justify-center w-full h-auto p-4 mt-8">
+        <h1 className="text-2xl md:text-4xl font-medium mb-6">Comment ça marche ?</h1>
+
+        <div className="flex flex-col md:flex-row items-center justify-center w-full h-auto text-center">
+          <div className="flex flex-col items-center justify-center w-full md:w-1/3 p-4">
+            <h2 className="text-xl md:text-2xl font-medium">1. Décrivez votre besoin</h2>
+            <p className="text-gray-700">
+              Décrivez votre besoin en quelques mots pour que nous puissions vous aider au mieux.
+            </p>
           </div>
-          <div className="flex flex-col items-center justify-center w-1/3 p-4">
-            <h2 className="text-2xl font-medium">2. Recevez des offres</h2>
-            <p className="text-gray-700">Recevez des offres de prestataires qualifiés en fonction de votre besoin.</p>
+          <div className="flex flex-col items-center justify-center w-full md:w-1/3 p-4">
+            <h2 className="text-xl md:text-2xl font-medium">2. Recevez des offres</h2>
+            <p className="text-gray-700">
+              Recevez des offres de prestataires qualifiés en fonction de votre besoin.
+            </p>
           </div>
-          <div className="flex flex-col items-center justify-center w-1/3 p-4">
-            <h2 className="text-2xl font-medium">3. Choisissez le meilleur</h2>
-            <p className="text-gray-700">Comparez les offres et choisissez le prestataire qui vous convient le mieux.</p>
+          <div className="flex flex-col items-center justify-center w-full md:w-1/3 p-4">
+            <h2 className="text-xl md:text-2xl font-medium">3. Choisissez le meilleur</h2>
+            <p className="text-gray-700">
+              Comparez les offres et choisissez le prestataire qui vous convient le mieux.
+            </p>
           </div>
         </div>
-
       </section>
 
-      <FAQAccordion  />
+      <FAQAccordion />
 
-      
+      <section className="relative flex flex-col md:items-start text-center md:text-left items-center justify-center w-full mt-8 min-h-[50vh] overflow-hidden p-4 bg-blue-300 rounded-xl">
+        <h1 className="text-2xl md:text-4xl font-bold mb-6 text-balance w-full md:w-2/3 text-white">
+          Des <span className="text-blue-950 font-serif italic">experts qualifiés</span> pour des besoins de tout type et de toute taille.
+        </h1>
 
+        <a
+          href="#"
+          className="bg-blue-950 text-white py-2 px-4 rounded-lg transition-colors hover:bg-blue-900"
+        >
+          Commencer maintenant
+        </a>
+        <Image
+          src="/h_char.png"
+          alt="Illustration"
+          width={300}
+          height={300}
+          className="absolute -bottom-12 right-1/2 md:right-1/6 md:flex hidden h-auto"
+        />
+      </section>
     </main>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,32 @@
 "use client"
-// import { useState } from "react";
+import { useState } from "react";
 import Image from "next/image";
 
-
 const Navbar = () => {
-    // const [isOpen, setIsOpen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
-        <nav className="z-10 flex items-center justify-between w-full fixed top-0 left-0 bg-white p-4 text-gray-500 font-medium border-b border-gray-200 pr-24 pl-24">
-            <Image src="/logo.png" alt="Helpify" width={64} height={64} className="h-16 w-16" />
-            <ul>
+        <nav className="z-10 flex items-center justify-between w-full fixed top-0 left-0 bg-white p-4 text-gray-500 font-medium border-b border-gray-200 pr-6 pl-6 md:pr-24 md:pl-24">
+            <Image src="/logo.svg" alt="Helpify" width={500} height={500} className="w-32 h-12 md:w-48 md:h-18" />
+            <ul className={`absolute top-full left-0 w-full bg-white md:static md:flex md:w-auto md:bg-transparent ${isMenuOpen ? "flex flex-col items-center pb-5 mb-24" : "hidden"}`}>
                 <li className="inline-block px-4 py-2 rounded hover:text-blue-500"><a href="">Home</a></li>
                 <li className="inline-block px-4 py-2 rounded hover:text-blue-500"><a href="">Services</a></li>
                 <li className="inline-block px-4 py-2 rounded hover:text-blue-500"><a href="">Devenir prestataire</a></li>
                 <li className="inline-block px-4 py-2 rounded hover:text-blue-500"><a href="">Se connecter</a></li>
                 <li className="inline-block px-4 py-2 text-black border rounded transition hover:bg-black hover:text-white"><a href="">S&#39;inscrire</a></li>
             </ul>
-
+            <button
+                className="block md:hidden text-gray-500 focus:outline-none"
+                onClick={toggleMenu}
+            >
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16m-7 6h7"></path>
+                </svg>
+            </button>
         </nav>
     );
 };
